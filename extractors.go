b330ea6bbfb6e43a1bf9268d0c792263e0ce629d
Patrick Bucher <patrick.bucher@mailbox.org>
2021-02-18 19:34:59 +0100

    additional extractor function ExtractChildrenTexts to extract all text children of a node

--- extractors.go
+++ extractors.go
@@ -1,6 +1,10 @@
 package htmlsqueeze
 
-import "golang.org/x/net/html"
+import (
+	"strings"
+
+	"golang.org/x/net/html"
+)
 
 // Extractor is a function that extracts text of a node.
 type Extractor func(n *html.Node) string
@@ -12,4 +16,17 @@ func ExtractChildText(n *html.Node) string {
 		return n.FirstChild.Data
 	}
 	return ""
+}
+
+// ExtractChildrenTexts returns the text of n's children that are text nodes
+// separated by space.
+func ExtractChildrenTexts(n *html.Node) string {
+	texts := make([]string, 0)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type != html.TextNode {
+			continue
+		}
+		texts = append(texts, strings.TrimSpace(c.Data))
+	}
+	return strings.Join(texts, " ")
 }
